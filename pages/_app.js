ac3ead48fca5e83fb8f19a5e1106df09d458587d
Anurag Kumar <anuragabcr@gmail.com>
2023-03-07 09:32:14 +0530

    completed according to requirements

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
